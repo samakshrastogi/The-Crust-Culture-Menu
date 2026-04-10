de9e25bd01fd7fd4590a8fa0154249d7117090d5
Samaksh Rastogi <124247791+samakshrastogi@users.noreply.github.com>
2026-04-10 17:47:10 +0530

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -15,7 +15,7 @@ const MENU_DATA = [
         title: "Everyday Classics",
         type: "3",
         labels: ["S", "M", "L"],
-        image: "images/everyday-classics.png",
+        image: "everyday-classics.png",
         items: [
             { name: "Classic", prices: ["49", "-", "-"] },
             { name: "Onion", prices: ["89", "179", "269"] },
@@ -31,7 +31,7 @@ const MENU_DATA = [
         title: "Classic Veg Combos",
         type: "3",
         labels: ["S", "M", "L"],
-        image: "images/classic-veg-combo.png",
+        image: "classic-veg-combo.png",
         items: [
             { name: "Golden Corn + Onion", prices: ["99", "189", "299"] },
             { name: "Onion + Capsicum", prices: ["99", "189", "299"] },
@@ -44,7 +44,7 @@ const MENU_DATA = [
         title: "Signature Veg Pizzas",
         type: "3",
         labels: ["S", "M", "L"],
-        image: "images/signature.png",
+        image: "signature.png",
         items: [
             { name: "Yammy Veg Pizza", prices: ["129", "229", "319"] },
             { name: "Maxican Pizza", prices: ["129", "229", "319"] },
@@ -61,7 +61,7 @@ const MENU_DATA = [
         title: "Royal Paneer Collection 👑",
         type: "3",
         labels: ["S", "M", "L"],
-        image: "images/royal-paneer.png",
+        image: "royal-paneer.png",
         items: [
             { name: "Paneer Capsicum", prices: ["149", "249", "319"] },
             { name: "Paneer Onion", prices: ["149", "249", "319"] },
@@ -78,7 +78,7 @@ const MENU_DATA = [
         title: "Garlic Breads & Sides 🥖",
         type: "1",
         labels: [""],
-        image: "images/garlic-bread.png",
+        image: "garlic-bread.png",
         items: [
             { name: "Garlic Bread", prices: ["99"] },
             { name: "Garlic Bread Stuffed", prices: ["149"] },
@@ -89,7 +89,7 @@ const MENU_DATA = [
         title: "Quick Wraps 🌯",
         type: "1",
         labels: [""],
-        image: "images/veg-parcel.png",
+        image: "veg-parcel.png",
         items: [
             { name: "Veg Parcel", prices: ["49"] },
             { name: "Indi Tandoori", prices: ["69"] }
@@ -99,7 +99,7 @@ const MENU_DATA = [
         title: "Grilled Sandwiches 🥪",
         type: "1",
         labels: [""],
-        image: "images/sandwhich.png",
+        image: "sandwhich.png",
         items: [
             { name: "Veg Grill Sandwich", prices: ["69"] },
             { name: "Veg, Golden Corn", prices: ["89"] },
@@ -113,7 +113,7 @@ const MENU_DATA = [
         title: "Burgers & Street Bites 🍔",
         type: "1",
         labels: [""],
-        image: "images/burgers.png",
+        image: "burgers.png",
         items: [
             { name: "Aloo Tikki Burger", prices: ["49"] },
             { name: "Vada Pav", prices: ["49"] },
@@ -130,7 +130,7 @@ const MENU_DATA = [
         title: "Maggi Mania 🍜",
         type: "2",
         labels: ["H", "F"],
-        image: "images/maggie.png",
+        image: "maggie.png",
         items: [
             { name: "Plain Maggie", prices: ["49", "89"] },
             { name: "Veggie Maggie", prices: ["59", "99"] },
@@ -144,7 +144,7 @@ const MENU_DATA = [
         title: "Momo Specials 🥟",
         type: "2",
         labels: ["H", "F"],
-        image: "images/momos.png",
+        image: "momos.png",
         items: [
             { name: "Veg Steam Momo", prices: ["49", "69"] },
             { name: "Veg Fried Momo", prices: ["59", "79"] },
@@ -156,7 +156,7 @@ const MENU_DATA = [
         title: "Fry Fiesta 🍟",
         type: "2",
         labels: ["H", "F"],
-        image: "images/fries.png",
+        image: "fries.png",
         items: [
             { name: "Classic Salted", prices: ["69", "129"] },
             { name: "Peri Peri Fries", prices: ["79", "129"] },
@@ -170,7 +170,7 @@ const MENU_DATA = [
         title: "Crispy Starters 🍢",
         type: "2",
         labels: ["H", "F"],
-        image: "images/crispy-starters.png",
+        image: "crispy-starters.png",
         items: [
             { name: "Spring Roll", prices: ["49", "99"] },
             { name: "Veg Nuggets", prices: ["69", "119"] },
@@ -183,7 +183,7 @@ const MENU_DATA = [
         title: "Indo-Chinese Specials 🥢",
         type: "2",
         labels: ["H", "F"],
-        image: "images/chinese.png",
+        image: "chinese.png",
         items: [
             { name: "Dry Manchurian", prices: ["99", "189"] },
             { name: "Chilli Potato", prices: ["119", "199"] },
@@ -195,7 +195,7 @@ const MENU_DATA = [
         title: "Stuffed Parathas 🫓",
         type: "1",
         labels: [""],
-        image: "images/paratha.png",
+        image: "paratha.png",
         items: [
             { name: "Aloo Paratha (2 Pieces)", prices: ["79"] },
             { name: "Mix Veg Paratha (2 Pieces)", prices: ["99"] },
@@ -206,7 +206,7 @@ const MENU_DATA = [
         title: "Wok Rice Bowls 🍚",
         type: "2",
         labels: ["H", "F"],
-        image: "images/rice.png",
+        image: "rice.png",
         items: [
             { name: "Veg Fried Rice", prices: ["69", "129"] },
             { name: "Schezwan", prices: ["89", "149"] },
@@ -219,7 +219,7 @@ const MENU_DATA = [
         title: "South Indian Specials 🥥",
         type: "1",
         labels: [""],
-        image: "images/south-indian.png",
+        image: "south-indian.png",
         items: [
             { name: "Idli Sambhar(2 Pieces)", prices: ["59"] },
             { name: "Sponge Dosa(2 Pieces)", prices: ["99"] },
@@ -231,7 +231,7 @@ const MENU_DATA = [
         title: "Tandoori Specials 🔥",
         type: "2",
         labels: ["H", "F"],
-        image: "images/malai-chaap.png",
+        image: "malai-chaap.png",
         items: [
             { name: "Malai Chaap", prices: ["129", "199"] }
         ]
@@ -240,7 +240,7 @@ const MENU_DATA = [
         title: "Hot & Cold Beverages ☕🥤",
         type: "1",
         labels: [""],
-        image: "images/drinks.png",
+        image: "drinks.png",
         items: [
             { name: "Ginger Tea", prices: ["19"] },
             { name: "Masala Chai", prices: ["29"] },
