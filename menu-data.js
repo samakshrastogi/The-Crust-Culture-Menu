3da14ec3f5e7e4b4121e631077c295e4e26ce7e8
Samaksh Rastogi <124247791+samakshrastogi@users.noreply.github.com>
2026-06-06 18:43:19 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- menu-data.js
+++ menu-data.js
@@ -24,7 +24,7 @@ const MENU_DATA = [
       { name: "Tomato", prices: ["89", "179", "269"] },
       { name: "Capsicum", prices: ["89", "179", "269"] },
       { name: "Golden Corn", prices: ["99", "189", "299"] },
-      { name: "Margherita", prices: ["99", "199", "319"] },
+      { name: "Margherita", prices: ["99", "219", "299"] },
       { name: "Golden Corn & Cheese", prices: ["129", "219", "329"] },
       { name: "Double Cheese Margherita", prices: ["129", "219", "329"] },
     ],
